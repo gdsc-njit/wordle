8308d92f80174104f6013723f02390b6c00a5cd3
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-10-05 16:42:02 -0400

    Update letter_state.py

--- letter_state.py
+++ letter_state.py
@@ -1,5 +1,5 @@
 class LetterState:
     def __init__(self, character: str):
         self.character = character
-        self.is_in_word = False
-        self.is_in_position = False
+        self.in_word = False
+        self.in_right_position = False
