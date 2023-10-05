e33647ac2a832260947e4c1b8fcc23a8f7c95f73
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-10-05 16:40:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def drawBorder(lines, size=9, pad=1):
 def convertWord2Color(result):
     result_color = []
     for letter in result:
-        if letter.is_in_position:
+        if letter.in_right_position:
             backgr = "on_green"
-        elif letter.is_in_word:
+        elif letter.in_word:
             backgr = "on_light_yellow"
         else:
             backgr = "on_red"
@@ -37,8 +37,7 @@ def convertWord2Color(result):
     return " ".join(result_color)
 
 def displayResult(wordle: Wordle):
-    print("Your results: ...")
-    print(f"You have {wordle.remaining_attempts()} attempts left.")
+    print(f"You have {wordle.remainingAttempts()} attempts left.")
 
     lines = []
     for word in wordle.attempts:
@@ -46,17 +45,17 @@ def displayResult(wordle: Wordle):
         colored_result_str = convertWord2Color(result)
         lines.append(colored_result_str)
 
-    for _ in range(wordle.remaining_attempts()):
+    for _ in range(wordle.remainingAttempts()):
         lines.append(" ".join(["_"] * wordle.WORD_LENGTH)) # - - - - - -
 
     drawBorder(lines)
 
 def main():
-    word_set = loadWordSet("dictionary.txt")
+    word_set = loadWordSet("src/dictionary.txt")
     word = getRandomWord(word_set)
     wordle = Wordle(word)
 
-    while wordle.can_attempt():
+    while wordle.stillAttempt():
         attempt = input("\nType your guess: ").upper()
         if len(attempt) != wordle.WORD_LENGTH:
             print(colored(f"Word must be {wordle.WORD_LENGTH} characters long!", "red"), end="")
@@ -73,5 +72,13 @@ def main():
     else:
         print(colored(f"WRONG. The answer is {word}!", "red"))
 
+def WordsDatabase():
+    with open("src/words.txt", "r") as source, open('src/dictionary.txt', 'w') as destination:
+        for word in source.readlines():
+            if len(word) == 6:
+                destination.write(word)
+    print("All words loaded.")
+
 if __name__ == "__main__":
+    # WordsDatabase()
     main()
